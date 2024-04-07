d4a0b78d6e6d8ee069e53cbfe8d373e4859b0934
Simizak <szymon_bor4@wp.pl>
2024-04-07 21:18:25 +0200

    added solving

--- src/path.h
+++ src/path.h
@@ -1,21 +1,6 @@
+#include <stdlib.h>
 #ifndef PATH_H
-struct Node{
-    int nodeX;
-    int nodeY;
-    int lenght;
-    char rotation;
-    struct Node *previous;
-};
 
-struct Point{
-    int X;
-    int Y;
-    char rotation;
-};
-
-
-struct Node pathFinder(struct Maze *maze);
-
-void pathGet(struct Node *nodes);
+void findShortestPath(struct Maze maze, int width, int height, FILE *file);
 
 #endif 
