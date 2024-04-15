d684d49399cba6dfcd2839dc616887d603a860a9
Simizak <szymon_bor4@wp.pl>
2024-04-15 11:15:06 +0200

    fixed solving

--- src/path.c
+++ src/path.c
@@ -3,11 +3,6 @@
 #include <stdbool.h>
 #include"maze.h"
 
-/*TO FIX
-- TRACKING BACK
-- VISITED MEMORY
-*/
-
 #define MAX_QUEUE_SIZE 512
 #define MAX_SIZE 1024
 
@@ -75,76 +70,112 @@ bool isValid(int x, int y, int width, int height) {
     return (x >= 0 && x < height && y >= 0 && y < width);
 }
 void findShortestPath(struct Maze maze, int width, int height, FILE* file) {
-    // Define the directions: up, down, left, right
+    // kierunki
     int dx[] = {-1, 1, 0, 0};
     int dy[] = {0, 0, -1, 1};
+    char rot[] = {'E','W','S','N'};
 
-    // Create a visited array
-    bool visited[MAX_SIZE][MAX_SIZE] = {false};
-
-    // Create a queue for BFS
     Queue* queue = createQueue();
 
     int entryX = maze.entry % maze.width;
     int entryY = maze.entry / maze.width;
 
-    // Mark the entry point as visited and enqueue it
     Position start = {entryX, entryY};
-    visited[entryX][entryY] = true;
     enqueue(queue, start);
 
     int exitX = maze.exit % maze.width;
     int exitY = maze.exit / maze.width;
 
-    // Perform BFS
+    // BFS
     while (!isEmpty(queue)) {
         Position current = dequeue(queue);
 
-        // Check if exit is reached
         if (current.x == exitX && current.y == exitY) {
             break;
         }
 
-        // Explore neighbors
         for (int i = 0; i < 4; i++) {
             int newX = current.x + dx[i];
             int newY = current.y + dy[i];
 
-            if (isValid(newX, newY, width, height) && *maze_get(&maze,newX,newY) == ' ' && !visited[newX][newY]) {
-                visited[newX][newY] = true;
+            if (isValid(newX, newY, width, height) && *maze_get(&maze,newX,newY) == ' ') {
                 enqueue(queue, (Position){newX, newY});
+                // skąd się tu dostał
+                *maze_get(&maze,newX,newY) = rot[i];
             }
         }
     }
-    /*
-    printf("Checked");
-    // Trace back the path and save it to the file
-    Position current = {exitX, exitY};
-    int count = 0;
-   while(current.x != entryX || current.y != entryY){
-        //visited[current.x][current.y] = false;
-        if(isValid(current.x + 1, current.y, width, height) && visited[current.x + 1][current.y]) {
-            printf("LEFT\n");
-            current.x++;
-        } else if (isValid(current.x, current.y + 1, width, height) && visited[current.x][current.y + 1]) {
-            printf("UP\n");
-            current.y++;
-        } else if (isValid(current.x - 1, current.y, width, height) && visited[current.x - 1][current.y]) {
-            printf("RIGHT\n");
-            current.x--;
-        } else if (isValid(current.x, current.y - 1, width, height) && visited[current.x][current.y - 1]) {
-            printf("DOWN\n");
-            current.y--;
+    
+    //Zaznaczenie najkrótszej ścieżki
+    int curX = exitX;
+    int curY = exitY;
+    while(curX != entryX || curY != entryY){
+        switch (*maze_get(&maze,curX,curY))
+        {
+        case 'N':
+            *maze_get(&maze,curX,curY) = 'F';
+            curY --;
+            break;
+        case 'S':
+            *maze_get(&maze,curX,curY) = 'F';
+            curY ++;
+            break;
+        case 'E':
+            *maze_get(&maze,curX,curY) = 'F';
+            curX ++;
+            break;
+        case 'W':
+            *maze_get(&maze,curX,curY) = 'F';
+            curX --;
+            break;
+        default:
+            break;
         }
-        count ++;
-   }
+    }
+    curX = entryX;
+    curY = entryY;
+    int count = 0;
 
-   printf("%i\n",count) ;*/
-
-    // Free allocated memory
+    //Wypisanie listy kroków
+    
+    fprintf(file,"START\n");
+    while(curX != exitX || curY != exitY){
+        if(isValid(curX + 1, curY, width, height) && *maze_get(&maze,curX+1,curY) == 'F'){
+            while(isValid(curX + 1, curY, width, height) && *maze_get(&maze,curX+1,curY) == 'F'){
+            curX ++;
+            count++;
+            *maze_get(&maze,curX,curY) = 'c';}
+            fprintf(file, "RIGHT %i\n", count);
+            count = 0;
+        }
+        if(isValid(curX - 1, curY, width, height) && *maze_get(&maze,curX -1,curY) == 'F'){
+            while(isValid(curX - 1, curY, width, height) && *maze_get(&maze,curX-1,curY) == 'F'){
+            curX --;
+            count++;
+            *maze_get(&maze,curX,curY) = 'c';}
+            fprintf(file, "LEFT %i\n", count);
+            count = 0;
+        }
+        if(isValid(curX, curY + 1, width, height) && *maze_get(&maze,curX,curY + 1) == 'F'){
+            while(isValid(curX, curY + 1, width, height) && *maze_get(&maze,curX,curY + 1) == 'F'){
+            curY++;
+            count++;
+            *maze_get(&maze,curX ,curY ) = 'c';}
+            fprintf(file, "DOWN %i\n", count);
+            count = 0;
+        }
+        if(isValid(curX, curY - 1, width, height) && *maze_get(&maze,curX,curY - 1) == 'F'){
+            while(isValid(curX, curY - 1, width, height) && *maze_get(&maze,curX,curY - 1) == 'F'){
+            curY--;
+            count++;
+            *maze_get(&maze,curX ,curY ) = 'c';}
+            fprintf(file, "UP %i\n", count);
+            count = 0;
+        }
+    }
+    
     while (!isEmpty(queue)) {
         dequeue(queue);
     }
     free(queue);
 }
-
